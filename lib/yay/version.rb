14d257e1ab0c06bfe15f2ca1b6355b1ff8f34e59
Jon Davey <jond3k@gmail.com>
2011-11-18 18:36:49 +0000

    Version had some weird indentation

--- lib/yay/version.rb
+++ lib/yay/version.rb
@@ -3,20 +3,20 @@ require 'open3'
 class Yay
 
   # the gem version. increment to make a new release!
-	VERSION = "0.0.5"
+  VERSION = "0.0.5"
 
   # yoinked from chef. this gives us the git revision number of the current
   # build if possible. used for --version
-	def self.version
-		@rev ||= begin
-			begin
-				rev = Open3.popen3("git rev-parse HEAD") {|stdin, stdout, stderr| stdout.read }.strip
-			rescue Errno::ENOENT
-				rev = ""
-			end
-			rev.empty? ? nil : " (#{rev})"
-		end
-		"#{VERSION}#@rev"
-	end
+  def self.version
+    @rev ||= begin
+      begin
+        rev = Open3.popen3("git rev-parse HEAD") {|stdin, stdout, stderr| stdout.read }.strip
+      rescue Errno::ENOENT
+        rev = ""
+      end
+      rev.empty? ? nil : " (#{rev})"
+    end
+    "#{VERSION}#@rev"
+  end
 
 end
