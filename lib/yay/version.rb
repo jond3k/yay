7cec34c12c40bee89b4441edb02dddede53afd75
Jon <jond3k@gmail.com>
2011-11-22 17:33:12 +0000

    Incremented version (Better default colour rules)

--- lib/yay/version.rb
+++ lib/yay/version.rb
@@ -3,7 +3,7 @@ require 'open3'
 class Yay
 
   # the gem version. increment to make a new release!
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 
   # yoinked from chef. this gives us the git revision number of the current
   # build if possible. used for --version
